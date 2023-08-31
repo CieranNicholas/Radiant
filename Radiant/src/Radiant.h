6996e6c75fd32c0643ad35ae934164882bd27165
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2023-08-31 23:25:29 +0100

    Setup application structure and entry point

---
+++ Radiant/src/Radiant.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// For use by Radiant applications
+#include "Radiant/Application.h"
+
+// -- Entry Point ----------------
+#include "Radiant/EntryPoint.h"
+// -------------------------------
