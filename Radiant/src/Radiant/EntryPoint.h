e23959fa0d00e0741b5e3a54f51a526fda9286e8
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2023-09-01 00:36:14 +0100

    Logging system

--- Radiant/src/Radiant/EntryPoint.h
+++ Radiant/src/Radiant/EntryPoint.h
@@ -6,9 +6,14 @@ extern Radiant::Application* Radiant::CreateApplication();
 	
 int main(int argc, char** argv)
 {
+	Radiant::Log::Init();
+	RD_CORE_WARN("Initialized Log!");
+	int a = 5;
+	RD_INFO("Hello! Var={0}", a);
+
 	auto app = Radiant::CreateApplication();
 	app->Run();
 	delete app;
 }
-
+	
 #endif
---
+++ Radiant/src/Radiant/Log.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h";
+#include "spdlog/spdlog.h";
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Radiant
+{
+	class RADIANT_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core log macros
+#define RD_CORE_INFO(...)	::Radiant::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define RD_CORE_TRACE(...)	::Radiant::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define RD_CORE_WARN(...)   ::Radiant::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define RD_CORE_ERROR(...)  ::Radiant::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define RD_CORE_CRITICAL(...)  ::Radiant::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+//Client log macros
+#define RD_INFO(...)	    ::Radiant::Log::GetClientLogger()->info(__VA_ARGS__)
+#define RD_TRACE(...)	    ::Radiant::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define RD_WARN(...)        ::Radiant::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define RD_ERROR(...)       ::Radiant::Log::GetClientLogger()->error(__VA_ARGS__)
+#define RD_CRITICAL(...)       ::Radiant::Log::GetClientLogger()->critical(__VA_ARGS__)
